a3ef557ffc5b369db4f70533162556e0a8912d99
Daniel Wilson <wilsonsdaniels@gmail.com>
2023-03-24 04:20:58 -0400

    first commit

---
+++ my_module.py
@@ -0,0 +1,10 @@
+# def printName(name):
+#     print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+
+import math
+
+def calculate_square_footage(length, width):
+    return length * width
+
+def calculate_circle_circumference(radius):
+    return 2 * math.pi * radius
